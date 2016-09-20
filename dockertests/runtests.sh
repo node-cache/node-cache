09e3b4a2c94d0c9c9d7f2466c12de9554910ace5
erdii <erdii@users.noreply.github.com>
2016-09-20 12:14:13 +0200

    updated dockertests [skip ci]

--- dockertests/runtests.sh
+++ dockertests/runtests.sh
@@ -1,12 +1,10 @@
 VERSIONS[0]="0.10"
 VERSIONS[1]="0.12"
 VERSIONS[2]="4.2"
-# currently expresso is not working on node 5.x
-#VERSIONS[3]="5.0"
-#VERSIONS[4]="5.4"
+VERSIONS[3]="5.0"
+VERSIONS[4]="5.4"
 VERSIONS[5]="lts"
-# currently expresso is not working on node 5.x
-#VERSIONS[6]="latest"
+VERSIONS[6]="latest"
 
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -25,7 +23,7 @@ do
 	   BUILDLOGS="$DIR/dockerbuild_$version.log"
 	   rm -f $BUILDLOGS
 	   docker build -t=mpneuried.nodecache.test.$version -f=$SCRIPTDIR/$DFile . > $BUILDLOGS
-	   docker run mpneuried.nodecache.test.$version >&2
+	   docker run --rm mpneuried.nodecache.test.$version >&2
    else
 	   echo "Dockerfile '$DFile' not found"
    fi
